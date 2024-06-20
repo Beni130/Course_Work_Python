e200ab64613a502297c361cfb0a3733eadeea459
Beni130 <139362357+Beni130@users.noreply.github.com>
2024-06-20 13:54:15 +0300

    Create Q10.py

---
+++ Q10.py
@@ -0,0 +1,22 @@
+Declare a function named sum_of_numbers. It takes a number parameter‬ and it adds all the numbers in that range.‬
+
+print(sum_of_numbers(5))  #15
+print(sum_all_numbers(10))  #55
+print(sum_all_numbers(100))  #5050
+
+Answer:
+
+def add_item(lst, item):
+    return lst + [item]
+
+food_staff = ['Potato', 'Tomato', 'Mango', 'Milk']
+print(add_item(food_staff, 'Meat'))
+numbers = [2, 3, 7, 9]
+print(add_item(numbers, 5))
+
+
+
+
+
+
+‭
