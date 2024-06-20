3c056f251f936be8842f5e3a45c074a20767c1ba
Beni130 <139362357+Beni130@users.noreply.github.com>
2024-06-20 14:46:25 +0300

    Create Q34.py

---
+++ Q34.py
@@ -0,0 +1,8 @@
+Q) Modify the siblings tuple and add the name of your father and mother and‬ assign it to family_members.
+
+Answer:
+
+family_members = siblings + ("Father", "Mother")
+print(family_members)
+
+‭
