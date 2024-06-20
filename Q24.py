7a02cf09ceebc383ca6e29c1ac492b571bc814ab
Beni130 <139362357+Beni130@users.noreply.github.com>
2024-06-20 14:29:12 +0300

    Create Q24.py

---
+++ Q24.py
@@ -0,0 +1,13 @@
+Q) Add name, color, breed, legs, age to the dog dictionary
+
+Answer:
+
+dog = {
+    "name": "Buddy",
+    "color": "Brown",
+    "breed": "Golden Retriever",
+    "legs": 4,
+    "age": 3
+}
+
+print(dog)
