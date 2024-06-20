1089ef3090fef4fd77c46b48a9575592fd7b3588
Beni130 <139362357+Beni130@users.noreply.github.com>
2024-06-20 14:31:00 +0300

    Create Q25.py

---
+++ Q25.py
@@ -0,0 +1,26 @@
+Q) Create a student dictionary and add first_name, last_name, gender, age,‬ marital status, skills, country, city and address as keys for the dictionary
+‭
+
+Answer:
+
+student = {
+    "first_name": "John",
+    "last_name": "Doe",
+    "gender": "Male",
+    "age": 20,
+    "marital_status": "Single",
+    "skills": ["Python", "Java", "C++"],
+    "country": "USA",
+    "city": "New York",
+    "address": "123 Main St"
+}
+
+print(student)
+
+
+
+
+
+
+
+
