8e2849e6776cc44420fe33ecd610d9aeb6e4b842
Beni130 <139362357+Beni130@users.noreply.github.com>
2024-06-20 13:49:13 +0300

    Create Q9.py

---
+++ Q9.py
@@ -0,0 +1,25 @@
+Q) Declare a function named add_item. It takes a list and an item parameters. It‬ returns a list with the item added at the end
+
+food_staff‬‭ = ['Potato', 'Tomato', 'Mango', 'Milk'];
+print(add_item(food_staff, 'Meat'))  # ['Potato', 'Tomato', 'Mango', 'Milk', 'Meat';
+numbers = [2, 3, 7, 9];
+print(add_item(numbers, 5))  [2, 3, 7, 9 5]
+
+
+Answer:
+
+
+def add_item(lst, item):
+    return lst + [item]
+
+food_staff = ['Potato', 'Tomato', 'Mango', 'Milk']
+print(add_item(food_staff, 'Meat'))
+numbers = [2, 3, 7, 9]
+print(add_item(numbers, 5))
+
+‭
+
+
+
+
+‭
