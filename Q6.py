b1d52e245cdee834720254d6a3ecbb5e63fce021
Beni130 <139362357+Beni130@users.noreply.github.com>
2024-06-20 13:25:17 +0300

    Create Q6.py

---
+++ Q6.py
@@ -0,0 +1,14 @@
+Q) Declare a function named print_list. It takes a list as a parameter and it prints‬ out each element of the list.
+‭
+
+
+answer:
+
+
+def print_list(lst):
+    for item in lst:
+        print(item)
+
+print_list([1, 2, 3, 4, 5])
+
+
