efaf63849383c2510552c4188080707487a47905
Beni130 <139362357+Beni130@users.noreply.github.com>
2024-06-20 14:07:19 +0300

    Create Q15.py

---
+++ Q15.py
@@ -0,0 +1,16 @@
+Q) Write a functions which checks if all items are unique in the list.
+
+
+
+Answer:
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+print(is_prime(5))
+print(is_prime(6))
