2f6232ceb4982103b7fce4f419a16aa17f19b51f
Beni130 <139362357+Beni130@users.noreply.github.com>
2024-06-20 14:14:44 +0300

    Create Q17.py

---
+++ Q17.py
@@ -0,0 +1,28 @@
+Q) ‭Write a loop that makes seven calls to print(), so we get on the output the‬ following triangle:
+‭
+#‬
+‭
+##‬
+‭
+###‬
+‭
+####‬
+‭
+#####‬
+‭
+######‬
+‭
+#######
+
+Answer:
+
+for i in range(1, 8):
+    print('#' * i)
+
+
+
+
+
+
+
+
